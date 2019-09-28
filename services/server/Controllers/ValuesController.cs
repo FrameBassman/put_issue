4789b8dd1193d163648b75427060e680ae7395c4
Dmitry Romashov <framebassman@gmail.com>
2019-09-28 10:22:28 +0300

    add watch

--- services/server/Controllers/ValuesController.cs
+++ services/server/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Server.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
         }
 
         // GET api/values/5
